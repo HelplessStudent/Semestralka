d632edaaa841fb74edb42fae6e057c9faf93444f
Michal <velbon123@gmai.com>
2021-11-07 16:59:52 +0100

    In client added if & else, added System.exit

--- src/Client.java
+++ src/Client.java
@@ -7,18 +7,28 @@ class Client
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
-        for(;;) {
-            String sentence = inFromUser.readLine();
-            sendData = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            clientSocket.send(sendPacket);
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-            String modifiedSentence = new String(receivePacket.getData());
-            System.out.println("FROM SERVER:" + modifiedSentence);
-            clientSocket.close();
+       
+        for (;;) {
+            // byte[] sendData = new byte[1024]; //sluzi na nacitanie dat a neskor aj
+            // odosielame
+            // byte[] receiveData = new byte[1024]; //sluzi na prijatie sprav
+            String sentence = inFromUser.readLine(); // tipujem ze do tohot nahravam to co pisem do clienta
+            // System.out.println(sentence);
+            if (sentence.equals("~")) {
+                clientSocket.close();
+                System.exit(0);
+            } else {
+                byte[] sendData = new byte[1024]; // sluzi na nacitanie dat a neskor aj odosielame
+                byte[] receiveData = new byte[1024]; // sluzi na prijatie sprav
+                sendData = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                clientSocket.send(sendPacket);
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
+                String modifiedSentence = new String(receivePacket.getData());
+                System.out.println("FROM SERVER:" + modifiedSentence);
+            }
+
         }
     }
 }
